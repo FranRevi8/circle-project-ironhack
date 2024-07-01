e35cb5fa529f2d921b7c76b4e9e526d276de6de0
FranRevi8 <fcorevidiego@gmail.com>
2024-07-01 12:37:46 +0200

    added burguer menu

---
+++ homepage/index.js
@@ -0,0 +1,33 @@
+const burguer = document.getElementById("burguer");
+const burguerMenu = document.getElementById("hidden-nav");
+
+burguer.addEventListener("click", () => {
+  if (
+    burguerMenu.innerHTML ===
+    `<ul class="hidden-nav hidden-nav-active">
+    <li><a href="./index.html">Home</a></li>
+    <li><a href="#projects">Projects</a></li>
+    <li><a href="#services">Services</a></li>
+  </ul>`
+  ) {
+    burguerMenu.innerHTML = `<ul class="hidden-nav">
+    <li><a href="./index.html">Home</a></li>
+    <li><a href="#projects">Projects</a></li>
+    <li><a href="#services">Services</a></li>
+  </ul>`;
+
+    burguer.innerHTML = `<div class="burguer-line"></div>
+        <div class="burguer-line"></div>
+        <div class="burguer-line"></div>`;
+  } else {
+    burguerMenu.innerHTML = `<ul class="hidden-nav hidden-nav-active">
+    <li><a href="./index.html">Home</a></li>
+    <li><a href="#projects">Projects</a></li>
+    <li><a href="#services">Services</a></li>
+  </ul>`;
+
+    burguer.innerHTML = `<div class="burguer-line-yellow"></div>
+        <div class="burguer-line-yellow"></div>
+        <div class="burguer-line-yellow"></div>`;
+  }
+});
